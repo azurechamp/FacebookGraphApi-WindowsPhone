f94cf84cd0bcb6c623ffd692a736ff07b18a11da
Muhammad92 <saadmehmood21@gmail.com>
2015-03-03 17:17:10 +0500

    Main Commit | DOne

---
+++ Facebook/MainPage.xaml.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+using Facebook.Resources;
+using Newtonsoft.Json;
+using System.Windows.Media.Imaging;
+using Microsoft.Phone.Tasks;
+using System.Device.Location;
+
+namespace Facebook
+{
+    public partial class MainPage : PhoneApplicationPage
+    {
+
+        //LocationDeclaration
+        double Lat;
+        double Lon;
+
+        public MainPage()
+        {
+            InitializeComponent();
+           
+        }
+
+        void getJson(string username) 
+        {
+            WebClient webclient = new WebClient();
+           
+            //In async you put the url of api 
+            //It is basically when you download the data from internet
+            webclient.DownloadStringAsync(new Uri("http://graph.facebook.com/"+username));
+            
+            //This very line would be executed when the data would be downloaded and serialized
+            //it has the method attached webclient_DownloadStringCompleted();
+            webclient.DownloadStringCompleted += webclient_DownloadStringCompleted;
+        }
+
+        void webclient_DownloadStringCompleted(object sender, DownloadStringCompletedEventArgs e)
+        {
+            var rootObject = JsonConvert.DeserializeObject<RootObject>(e.Result);
+            
+            
+            CoverImage.Source = new BitmapImage(new Uri(rootObject.cover.source));
+            City.Text = rootObject.location.city;
+            Country.Text = rootObject.location.country;
+            Ablut.Text = rootObject.about;
+            likes.Text = rootObject.likes.ToString();
+            Checkins.Text = rootObject.checkins.ToString();
+            website.Text = rootObject.website;
+            link.Text = rootObject.link;
+            Lat = rootObject.location.latitude;
+            Lon = rootObject.location.longitude;
+
+
+
+
+                
+        }
+
+
+
+        
+        private void Enter_Click(object sender, RoutedEventArgs e)
+        {
+            getJson(input.Text);
+            
+        }
+
+        private void naviagte_Click(object sender, RoutedEventArgs e)
+        {
+             
+
+            BingMapsTask bindmapTask = new BingMapsTask();
+            bindmapTask.Center = new GeoCoordinate
+            {
+             Latitude = Lat, Longitude = Lon
+            };
+            bindmapTask.Show();
+        }
+    }
+}
---
+++ Facebook/RootObject.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Facebook
+{
+
+    public class CategoryList
+    {
+        public string id { get; set; }
+        public string name { get; set; }
+    }
+
+    public class Cover
+    {
+        public long cover_id { get; set; }
+        public int offset_x { get; set; }
+        public int offset_y { get; set; }
+        public string source { get; set; }
+        public string id { get; set; }
+    }
+
+    public class Location
+    {
+        public string city { get; set; }
+        public string country { get; set; }
+        public double latitude { get; set; }
+        public double longitude { get; set; }
+        public string zip { get; set; }
+    }
+
+    public class Parking
+    {
+        public int lot { get; set; }
+        public int street { get; set; }
+        public int valet { get; set; }
+    }
+
+    public class RootObject
+    {
+        public string id { get; set; }
+        public string about { get; set; }
+        public string awards { get; set; }
+        public bool can_post { get; set; }
+        public string category { get; set; }
+        public List<CategoryList> category_list { get; set; }
+        public int checkins { get; set; }
+        public Cover cover { get; set; }
+        public bool has_added_app { get; set; }
+        public bool is_community_page { get; set; }
+        public bool is_published { get; set; }
+        public int likes { get; set; }
+        public string link { get; set; }
+        public Location location { get; set; }
+        public string name { get; set; }
+        public Parking parking { get; set; }
+        public string phone { get; set; }
+        public int talking_about_count { get; set; }
+        public string username { get; set; }
+        public string website { get; set; }
+        public int were_here_count { get; set; }
+    }
+}
